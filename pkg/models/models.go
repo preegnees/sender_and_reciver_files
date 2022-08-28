904b7b02097bfa6b45d0f0780be981d549d2832e
preegnees <mrraziapov13@gmail.com>
2022-08-28 20:27:46 +0300

    добавил конфиг для ридера, так как там слишком много параметров

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -3,24 +3,29 @@ package models
 import (
 	"context"
 	"io"
-) 
+	"os"
+)
 
 type SettingsReaderOfFile struct {
-	Ctx context.Context
-	IReaderOfConf
-	BufferSize int64
+	Ctx         context.Context
+	PathToFile  string
+	ParentDir   string
+	BufferSize  int64
 	OptionsSize int64
-	Writer io.Writer
-}
-
-type Conf struct {
-	Path string
-}
-
-type IReaderOfConf interface {
-	Get() (*Conf, error)
+	Writer      io.Writer
 }
 
 type IReaderOfFile interface {
 	ReadFile(SettingsReaderOfFile) error
+}
+
+type ConfForReader struct {
+	Ctx context.Context
+	BufferSize int64
+	OptionsSize int64
+	FileSize int64
+	File *os.File
+	RelativePath string
+	ErrCh chan<- error
+	Writer io.Writer
 }
