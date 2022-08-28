b7be8534aef58a3a5d70878795e08bee6ef4c87f
preegnees <mrraziapov13@gmail.com>
2022-08-28 20:34:35 +0300

    изменил файл в соответвии с новыми входыми параметрами, новый конфиг для ридера, для контроллера файлов прописал функцию инициализации

--- pkg/sender/tests/reader_test.go
+++ pkg/sender/tests/reader_test.go
@@ -1,7 +1,6 @@
 package tests
 
 import (
-	"bufio"
 	"context"
 	"fmt"
 	"io"
@@ -13,6 +12,7 @@ import (
 
 	common "files/pkg/common"
 	reader "files/pkg/sender/reader"
+	models "files/pkg/models"
 )
 
 func TestRead_TrueSettings(t *testing.T) {
@@ -24,18 +24,12 @@ func TestRead_TrueSettings(t *testing.T) {
 	wg.Add(1)
 
 	errCh := make(chan error)
-	// path := "D:\\downloaded\\LibreOffice\\LibreOffice_7.3.5_Win_x64.msi"
 	path := "C:\\Users\\secrr\\Desktop\\work_with_files\\example\\send.txt"
 	file, _ := os.Open(path)
 	stat, _ := file.Stat()
 	size := stat.Size()
 
 	go func() {
-		// нужно так же написать шутку которая бы возварщая нужные открытые файлы, а потом закрывала
-		// path := "C:\\Users\\secrr\\Desktop\\work_with_files\\example\\recive.txt"
-		// file, _ := os.OpenFile(path, os.O_WRONLY|os.O_CREATE, 0777)
-		// w := bufio.NewWriter(file)
-		defer file.Close()
 		for {
 			select {
 			case err := <-errCh:
@@ -51,51 +45,73 @@ func TestRead_TrueSettings(t *testing.T) {
 			}
 		}
 	}()
-
-	reader.Read(ctx, 8, 256, size, file, errCh, pw) // 100 * 1024 * 1024 == 100 mb
+	paretDir := "C:\\Users\\secrr\\Desktop\\work_with_files\\example"
+	pathToFile := "C:\\Users\\secrr\\Desktop\\work_with_files\\example\\send.txt"
+	relativePath := common.GetRelativePath(paretDir, pathToFile)
+	cnf := models.ConfForReader{
+		Ctx: ctx,
+		BufferSize: 256,
+		OptionsSize: 256,
+		FileSize: size,
+		File: file,
+		RelativePath: relativePath,
+		ErrCh: errCh,
+		Writer: pw,
+	}
+	reader.Read(cnf) // 100 * 1024 * 1024 == 100 mb
 	pw.Close()
 	wg.Wait()
+	pr.Close()
 
 	stop := time.Now().UnixMilli()
 	log.Println("время выполнения:", stop-start)
 }
 
 type controllerOfFiles struct {
-	storage map[string](*os.File)
+	storage   map[string](*os.File)
+	parentDir string
 }
 
-func (fc *controllerOfFiles) writeFile(path string, data []byte) (nb int, err error) {
-	file, ok := fc.storage[path]
+func (cof *controllerOfFiles) writeToFile(path string, data []byte) (nb int, err error) {
+	file, ok := cof.storage[path]
 	if !ok {
-		file, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0777)
+		f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0777)
 		if err != nil {
-			return 0, fmt.Errorf("$Ошибка при открытии файла (получение файла из хранилища), err:=%v", err)
+			return 0, fmt.Errorf("$Ошибка при открытии файла (файла не было в хронилище), err:=%v", err)
 		}
-		fc.storage[path] = file
+		cof.storage[path] = f
+		file = f
+
 	}
-	w := bufio.NewWriter(file)
-	n, err := w.Write(data)
+	
+	n, err := file.Write(data)
 	if err != nil {
-		return n, fmt.Errorf("$Ошибка при записи в файл=%s, err:=%v", path, err)
+		return 0, fmt.Errorf("$Ошибка при записи в файл=%s, err:=%v", path, err)
 	}
 
 	return n, nil
 }
 
-func (fc *controllerOfFiles) closeAllFiles() {
-	for _, v := range fc.storage {
-		v.Close()
+func (cof *controllerOfFiles) closeAllFiles() {
+	for _, file := range cof.storage {
+		file.Close()
 	}
 }
 
-func (fc *controllerOfFiles) closeFile(path string) error {
-	file, ok := fc.storage[path]
+func (cof *controllerOfFiles) closeFile(path string) error {
+	file, ok := cof.storage[path]
 	if ok {
 		file.Close()
 		return nil
 	} else {
-		return fmt.Errorf("$Ошибка при закрытии файла=%s", path)
+		return fmt.Errorf("$Ошибка при закрытии файла=%s, видимо, такого файла нет", path)
 	}
+}
+
+func (cof *controllerOfFiles) New() *controllerOfFiles {
+	return &controllerOfFiles{
+		storage: make(map[string]*os.File),
+	} 
 }
 
 type Reader interface {
@@ -103,22 +119,22 @@ type Reader interface {
 }
 type FileDistributor struct{}
 
+var cof controllerOfFiles = controllerOfFiles{}
+var controller = cof.New() 
+
 var _ io.Writer = (*FileDistributor)(nil)
 
-var fc controllerOfFiles = controllerOfFiles{
-	storage: make(map[string]*os.File),
-}
-
 func (r *FileDistributor) Write(buf []byte) (n int, err error) {
-	
 	fmt.Printf("%s\n", buf)
-	offset, path, index, sizeOpt := common.DeconstructOptions(buf)
+	offset, path, index, sizeOpt, sizeFile, sizeBuffer := common.DeconstructOptions(buf)
 	log.Println("offset:", offset)
 	log.Println("path:", path)
 	log.Println("index:", index)
 	log.Println("sizeOpt:", sizeOpt)
+	log.Println("sizeFile:", sizeFile)
+	log.Println("sizeBuffer:", sizeBuffer)
 	log.Printf("message: %s\n", buf[:len(buf)-int(sizeOpt)])
-	n, err = fc.writeFile(path, buf[:len(buf)-int(sizeOpt)])
+	n, err = controller.writeToFile(path, buf[:len(buf)-int(sizeOpt)])
 	log.Println(err)
 	if err != nil {
 		return n, err
